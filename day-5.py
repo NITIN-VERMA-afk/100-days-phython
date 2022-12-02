168c3e172a77be1fd53224f3992f7b4b1b645eb5
nitin verma <nitinvermamv61506@gmail.com>
2022-12-02 23:10:21 +0530

    fifth commit

---
+++ day-5.py
@@ -0,0 +1,9 @@
+# this is comment
+print("i m good programer\n and i become best programer in world\nand its my destiny")
+''' this
+ is
+  multi line 
+comment'''
+# /n is backslace escape sequence it create new line
+# \"" is also backscape sequence character
+
