c3bcb7df0b78879f65fe70a112837ec263ecebbc
nitin verma <nitinvermamv61506@gmail.com>
2022-12-03 19:47:19 +0530

    sixth-day

---
+++ day-6.py
@@ -0,0 +1,28 @@
+
+# mini project quiz game
+print("welcome to this quiz")
+playing =input("do you want to  play ")
+
+if playing != "yes":
+    quit()
+    print("okey!lets play :)")
+
+    answer = input ("what does cpu stand for ?")
+    if answer =="center processing unit":
+        print('answer is correct')
+    else:
+        print("incorrect!")
+        
+        answer = input ("what does gpu stand for ?")
+    if answer =="graphics processing unit":
+        print('answer is correct')
+    else:
+        print("incorrect!")
+
+
+
+
+
+
+
+    
