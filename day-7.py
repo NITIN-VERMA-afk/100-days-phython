2c571b52919a17006e374421216f5eebe72e2602
nitin verma <nitinvermamv61506@gmail.com>
2022-12-04 18:39:24 +0530

    7th-day

---
+++ day-7.py
@@ -0,0 +1,21 @@
+# addition of two numbers
+num1=int(input("enter number 1:"))
+num2=int(input("enter number 2:"))
+result = num1+num2
+print(result)
+# substraction of two numbers
+num1=int(input("enter number 1:"))
+num2=int(input("enter number 2:"))
+result = num1-num2
+print(result)
+# multipcations
+num1=int(input("enter number 1:"))
+num2=int(input("enter number 2:"))
+result = num1*num2
+print(result)
+# division 
+num1=int(input("enter number 1:"))
+num2=int(input("enter number 2:"))
+result = num1/num2
+print(result)
+# calculator
