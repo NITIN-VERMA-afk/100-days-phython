30d056df06a7b529afeb793f4b5f8be44090ad8b
nitin verma <nitinvermamv61506@gmail.com>
2022-12-08 21:06:46 +0530

    11-th day

---
+++ 11th-day.py
@@ -0,0 +1,14 @@
+# string
+name="nitin"
+friend ="none"
+print("hello"+ name,"and his friend"+friend)
+
+name2='rohit'
+print(name2)
+
+str=''' hlo my name is nitin and its all about fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff'''
+print(str)
+print(name[0])
+apple ="hello nitin"
+# for character in nitin:
+#     print(character)
