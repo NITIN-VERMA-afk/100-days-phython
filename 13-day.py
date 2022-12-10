78090d09a6b83468215144d712db24cf25859154
nitin verma <nitinvermamv61506@gmail.com>
2022-12-10 18:07:16 +0530

    13thday

---
+++ 13-day.py
@@ -0,0 +1,15 @@
+# string methods
+name="nitin"
+print(len(name))
+print(name.upper())
+print(name.lower())
+print(name.rstrip("!"))
+print(name.replace("nitin","john"))
+print(name.split(" "))
+blogheading="intrudeceing to js"
+print(blogheading.capitalize())
+str1="welcome to console"
+print(len(str1.center(50)))
+print(name.count("nitin"))
+print(name.find("nitin"))
+print(str1.index("to"))
