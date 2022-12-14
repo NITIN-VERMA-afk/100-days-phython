bd7dfd7baf5d6dda01df87adc2b1046e83664e87
nitin verma <nitinvermamv61506@gmail.com>
2022-12-14 20:45:58 +0530

    day15

---
+++ 14day.py
@@ -0,0 +1,18 @@
+# match statements
+
+x =input("enter the value of x")
+match x:
+    case 0:
+        print("x is zero")
+    case 1:
+        print("x is 1")
+    case 2:
+        print("xis not 3")
+
+
+
+
+
+
+
+
---
+++ day-15.py
@@ -0,0 +1,13 @@
+# name="nitin"
+# for i in name:
+#     print(i)
+
+# if(i=="b"):
+#     print("this is b")
+# colors=["red","blue","green"]
+# for color in colors:
+#     print(colors)
+#     for i in color:
+#         print(i)
+# for k in range(1,12,3):
+#     print(k)
